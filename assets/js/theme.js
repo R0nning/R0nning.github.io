ef93664b8725e807c337b320c5eaf7122714c3a7
Ron Beitler <axontide@gmail.com>
2017-06-08 16:09:50 -0400

    update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,5 +1,18 @@
-$('.grid').masonry({
-  itemSelector: '.grid-item',
-  columnWidth: '.grid-sizer',
-  percentPosition: true
-});
++var $grid = $('.grid');
+ +
+ +function triggerIsotope() {
+ +  if (! $grid) { return; }
+ +
+ +  $grid.isotope({
+ +      itemSelector: '.grid-item',
+ +      layoutMode: 'masonry',
+ +      masonry: {
+ +       columnWidth: 0
+ +      }
+ +  });
+ +
+ +}
+ +
+ +$(document).ready(function(){
+ +  triggerIsotope();
+ +}); 
