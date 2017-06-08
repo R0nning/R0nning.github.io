49723fe057afcec4e6fd7fa0d27537ab7b68bdbe
Ron Beitler <axontide@gmail.com>
2017-06-08 16:26:32 -0400

    Create theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,18 +1,18 @@
-+var $grid = $('.grid');
- +
- +function triggerIsotope() {
- +  if (! $grid) { return; }
- +
- +  $grid.isotope({
- +      itemSelector: '.grid-item',
- +      layoutMode: 'masonry',
- +      masonry: {
- +       columnWidth: 0
- +      }
- +  });
- +
- +}
- +
- +$(document).ready(function(){
- +  triggerIsotope();
- +}); 
+var $grid = $('.grid');
+ 
+function triggerIsotope() {
+  if (! $grid) { return; }
+
+  $grid.isotope({
+      itemSelector: '.grid-item',
+      layoutMode: 'masonry',
+      masonry: {
+       columnWidth: 0
+      }
+  });
+
+}
+
+$(document).ready(function(){
+  triggerIsotope();
+}); 
