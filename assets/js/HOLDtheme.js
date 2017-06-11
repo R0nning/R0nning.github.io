1909dbf905c62b23ca7fba5afeafef607008e67e
Ron Beitler <axontide@gmail.com>
2017-06-11 10:47:24 -0400

    Update HOLDtheme.js

--- assets/js/HOLDtheme.js
+++ assets/js/HOLDtheme.js
@@ -1,20 +1,49 @@
 $(document).ready(function(){
 
- var $grid = $('.grid').isotope({
-   itemSelector: '.grid-item',
-layoutMode: 'packery'
-      });
-  
+  var $grid = $('.grid');
+
+  function initMasonry() {
+    $grid.isotope({
+      itemSelector: '.grid-item',
+      layoutMode: 'packery',
+      packery: {
+        columnWidth: '.grid-sizer',
+        gutter: '.gutter-sizer',
+        percentPosition: true
+      }
+    });
+
     // layout Isotope after all images have loaded
-   // $grid.imagesLoaded(function() {
-    //   $grid.isotope({
-   //     //options
-    //   });
-    // });
+    $grid.imagesLoaded(function() {
+      $grid.isotope({
+        itemSelector: '.grid-item',
+        layoutMode: 'packery',
+        packery: {
+          columnWidth: '.grid-sizer',
+          gutter: '.gutter-sizer',
+          percentPosition: true
+        }
+      });
+    });
 
+    $('#filters').on('click', 'button', function() {
+      var filterVal = $(this).attr('data-filter');
+      $grid.isotope({ filter: filterVal});
+      $grid.isotope('shuffle');
+      
+      if (!$(this).hasClass('shuffle')){
+        $('#filters > button').removeClass('active');
+        $(this).addClass('active');
+      }
+    });
 
-$('#filters').on('click', 'button', function() {
-   var filterVal = $(this).attr('data-filter');
-   $grid.isotope({ filter: filterVal});
-  });
-}); 
+    $('.shuffle').on('click', function() {
+      $('#filters > button').removeClass('active');
+      $('button.all').addClass('active');
+      $grid.isotope('shuffle');  
+    });
+
+  }
+  
+  initMasonry();
+});
